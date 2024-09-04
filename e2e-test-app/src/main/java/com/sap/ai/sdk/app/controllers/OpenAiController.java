89bb17c1140b298154eafeb3ae30e2b363d9c8ec
Charles Dubois <103174266+CharlesDuboisSAP@users.noreply.github.com>
2024-09-04 16:58:52 +0200

    OpenAI Streaming (#25)
    * OpenAI streaming
    
    * Added homepage and error handling todo
    
    * Renamed vars
    
    * Added todos
    
    * Made stream generic, try-with resources, TEXT_EVENT_STREAM, exception refactored
    
    * Formatting
    
    * close stream correctly
    
    * Formatting
    
    * Created OpenAiStreamOutput
    
    * Formatting
    
    * Renamed stream to streamChatCompletion, Added comments
    
    * Added total output
    
    * Total output is printed
    
    * Formatting
    
    * addDelta is propagated everywhere
    
    * addDelta is propagated everywhere
    
    * forgotten addDeltas
    
    * Added jackson dependencies
    
    * Added Javadoc
    
    * Removed 1 TODO
    
    * PMD
    
    * PMD again
    
    * Added OpenAiClientTest.streamChatCompletion()
    
    * Change return type of stream, added e2e test
    
    * Added documentation
    
    * Added documentation framework-agnostic + throw if finish reason is invalid
    
    * Added error handling test
    
    * Updates from pair review / discussion
    
    * Cleanup + streamChatCompletion doesn't throw
    
    * PMD
    
    * Added errorHandling test
    
    * Apply suggestions from code review
    
    Co-authored-by: Matthias Kuhr <52661546+MatKuhr@users.noreply.github.com>
    
    * Dependency analyze
    
    * Review comments
    
    * Make client static
    
    * Formatting
    
    * PMD
    
    * Fix tests
    
    * Removed exception constructors no args
    
    * Refactor exception message
    
    * Readme sentences
    
    * Remove superfluous call super
    
    * reset httpclient-cache and -factory after each test case
    
    * Very minor code-style improvements in test
    
    * Minor code-style in OpenAIController
    
    * Reduce README sample code
    
    * Update OpenAiStreamingHandler.java (#43)
    
    * Fix import
    
    * Added stream_options to model
    
    * Change Executor#submit() to #execute()
    
    * Added usage testing
    
    * Added beautiful Javadoc to enableStreaming
    
    * typo
    
    * Fix mistake
    
    * streaming readme (#48)
    
    * Reduce sample code
    
    * Format
    
    ---------
    
    Co-authored-by: SAP Cloud SDK Bot <cloudsdk@sap.com>
    Co-authored-by: Matthias Kuhr <matthias.kuhr@sap.com>
    Co-authored-by: Matthias Kuhr <52661546+MatKuhr@users.noreply.github.com>
    Co-authored-by: Alexander Dümont <alexander.duemont@sap.com>
    Co-authored-by: Alexander Dümont <22489773+newtork@users.noreply.github.com>

--- e2e-test-app/src/main/java/com/sap/ai/sdk/app/controllers/OpenAiController.java
+++ e2e-test-app/src/main/java/com/sap/ai/sdk/app/controllers/OpenAiController.java
@@ -5,6 +5,8 @@ import static com.sap.ai.sdk.foundationmodels.openai.OpenAiModel.GPT_4O;
 import static com.sap.ai.sdk.foundationmodels.openai.OpenAiModel.TEXT_EMBEDDING_ADA_002;
 import static com.sap.ai.sdk.foundationmodels.openai.model.OpenAiChatCompletionTool.ToolType.FUNCTION;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sap.ai.sdk.foundationmodels.openai.OpenAiClient;
 import com.sap.ai.sdk.foundationmodels.openai.model.OpenAiChatCompletionFunction;
 import com.sap.ai.sdk.foundationmodels.openai.model.OpenAiChatCompletionOutput;
@@ -14,13 +16,21 @@ import com.sap.ai.sdk.foundationmodels.openai.model.OpenAiChatMessage.OpenAiChat
 import com.sap.ai.sdk.foundationmodels.openai.model.OpenAiChatMessage.OpenAiChatUserMessage.ImageDetailLevel;
 import com.sap.ai.sdk.foundationmodels.openai.model.OpenAiEmbeddingOutput;
 import com.sap.ai.sdk.foundationmodels.openai.model.OpenAiEmbeddingParameters;
+import com.sap.cloud.sdk.cloudplatform.thread.ThreadContextExecutors;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import javax.annotation.Nonnull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 
 /** Endpoints for OpenAI operations */
+@Slf4j
 @RestController
 class OpenAiController {
   /**
@@ -36,6 +46,98 @@ class OpenAiController {
             .setMessages(List.of(new OpenAiChatUserMessage().addText("Who is the prettiest")));
 
     return OpenAiClient.forModel(GPT_35_TURBO).chatCompletion(request);
+  }
+
+  /**
+   * Asynchronous stream of an OpenAI chat request
+   *
+   * @return the emitter that streams the assistant message response
+   */
+  @SuppressWarnings("unused") // The end-to-end test doesn't use this method
+  @GetMapping("/streamChatCompletionDeltas")
+  @Nonnull
+  public static ResponseEntity<ResponseBodyEmitter> streamChatCompletionDeltas() {
+    final var msg = "Can you give me the first 100 numbers of the Fibonacci sequence?";
+    final var request =
+        new OpenAiChatCompletionParameters()
+            .setMessages(List.of(new OpenAiChatUserMessage().addText(msg)));
+
+    final var stream = OpenAiClient.forModel(GPT_35_TURBO).streamChatCompletionDeltas(request);
+
+    final var emitter = new ResponseBodyEmitter();
+
+    final Runnable consumeStream =
+        () -> {
+          final var totalOutput = new OpenAiChatCompletionOutput();
+          // try-with-resources ensures the stream is closed
+          try (stream) {
+            stream
+                .peek(totalOutput::addDelta)
+                .forEach(delta -> send(emitter, delta.getDeltaContent()));
+          } finally {
+            send(emitter, "\n\n-----Total Output-----\n\n" + objectToJson(totalOutput));
+            emitter.complete();
+          }
+        };
+
+    ThreadContextExecutors.getExecutor().execute(consumeStream);
+
+    // TEXT_EVENT_STREAM allows the browser to display the content as it is streamed
+    return ResponseEntity.ok().contentType(MediaType.TEXT_EVENT_STREAM).body(emitter);
+  }
+
+  private static String objectToJson(@Nonnull final Object obj) {
+    try {
+      return new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(obj);
+    } catch (final JsonProcessingException ignored) {
+      return "Could not parse object to JSON";
+    }
+  }
+
+  /**
+   * Asynchronous stream of an OpenAI chat request
+   *
+   * @return the emitter that streams the assistant message response
+   */
+  @SuppressWarnings("unused") // The end-to-end test doesn't use this method
+  @GetMapping("/streamChatCompletion")
+  @Nonnull
+  public static ResponseEntity<ResponseBodyEmitter> streamChatCompletion() {
+    final var request =
+        new OpenAiChatCompletionParameters()
+            .setMessages(
+                List.of(
+                    new OpenAiChatUserMessage()
+                        .addText(
+                            "Can you give me the first 100 numbers of the Fibonacci sequence?")));
+
+    final var stream = OpenAiClient.forModel(GPT_35_TURBO).streamChatCompletion(request);
+
+    final var emitter = new ResponseBodyEmitter();
+
+    final Runnable consumeStream =
+        () -> {
+          try (stream) {
+            stream.forEach(deltaMessage -> send(emitter, deltaMessage));
+          } finally {
+            emitter.complete();
+          }
+        };
+
+    ThreadContextExecutors.getExecutor().execute(consumeStream);
+
+    // TEXT_EVENT_STREAM allows the browser to display the content as it is streamed
+    return ResponseEntity.ok().contentType(MediaType.TEXT_EVENT_STREAM).body(emitter);
+  }
+
+  private static void send(
+      @Nonnull final ResponseBodyEmitter emitter, @Nonnull final String chunk) {
+    try {
+      emitter.send(chunk);
+    } catch (final IOException e) {
+      log.error(Arrays.toString(e.getStackTrace()));
+      emitter.completeWithError(e);
+    }
   }
 
   /**
---
+++ foundation-models/openai/src/main/java/com/sap/ai/sdk/foundationmodels/openai/model/StreamedDelta.java
@@ -0,0 +1,43 @@
+package com.sap.ai.sdk.foundationmodels.openai.model;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/**
+ * Interface for streamed delta classes.
+ *
+ * <p>This interface defines a method to retrieve the content from a delta, which is a chunk in a
+ * stream of data. Implementations of this interface should provide the logic to extract the
+ * relevant content from the delta.
+ */
+public interface StreamedDelta {
+
+  /**
+   * Get the message content from the delta.
+   *
+   * <p>Note: If there are multiple choices only the first one is returned
+   *
+   * <p>Note: The first two and the last delta do not contain any content
+   *
+   * @return the message content or empty string.
+   */
+  @Nonnull
+  String getDeltaContent();
+
+  /**
+   * Reason for finish. The possible values are:
+   *
+   * <p>{@code stop}: API returned complete message, or a message terminated by one of the stop
+   * sequences provided via the stop parameter
+   *
+   * <p>{@code length}: Incomplete model output due to max_tokens parameter or token limit
+   *
+   * <p>{@code function_call}: The model decided to call a function
+   *
+   * <p>{@code content_filter}: Omitted content due to a flag from our content filters
+   *
+   * <p>{@code null}: API response still in progress or incomplete
+   */
+  @Nullable
+  String getFinishReason();
+}
