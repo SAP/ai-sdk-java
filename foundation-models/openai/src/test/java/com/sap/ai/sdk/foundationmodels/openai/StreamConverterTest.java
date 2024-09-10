20cc897fed7310a79253471926fa258080faab39
Alexander Dümont <alexander_duemont@web.de>
2024-09-10 15:33:13 +0200

    Add assertion on stream count

--- foundation-models/openai/src/test/java/com/sap/ai/sdk/foundationmodels/openai/StreamConverterTest.java
+++ foundation-models/openai/src/test/java/com/sap/ai/sdk/foundationmodels/openai/StreamConverterTest.java
@@ -11,6 +11,7 @@ import static org.mockito.Mockito.verify;
 
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.atomic.AtomicInteger;
 import lombok.SneakyThrows;
 import org.apache.hc.core5.http.ContentType;
 import org.apache.hc.core5.http.io.entity.InputStreamEntity;
@@ -26,14 +27,19 @@ public class StreamConverterTest {
     final var entity = new InputStreamEntity(inputStream, ContentType.TEXT_PLAIN);
 
     final var sut = StreamConverter.streamLines(entity);
-
     verify(inputStream, never()).read();
     verify(inputStream, never()).read(any());
     verify(inputStream, never()).read(any(), anyInt(), anyInt());
 
-    sut.forEach(
-        s -> assertThat(s).containsAnyOf("THIS", "IS", "A", "TEST").doesNotContainAnyWhitespaces());
+    final var streamCounter = new AtomicInteger(0);
+    sut.peek(s -> streamCounter.incrementAndGet())
+        .forEach(
+            s ->
+                assertThat(s)
+                    .containsAnyOf("THIS", "IS", "A", "TEST")
+                    .doesNotContainAnyWhitespaces());
 
+    assertThat(streamCounter).hasValue(StreamConverter.BUFFER_SIZE * 4);
     verify(inputStream, times(TEMPLATE.length() + 1))
         .read(any(), anyInt(), eq(StreamConverter.BUFFER_SIZE));
   }
