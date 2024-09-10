67a048983105a5e68ef9a7d581447eb78f755fa1
Alexander Dümont <alexander_duemont@web.de>
2024-09-10 15:24:09 +0200

    Add test

---
+++ foundation-models/openai/src/test/java/com/sap/ai/sdk/foundationmodels/openai/StreamConverterTest.java
@@ -0,0 +1,40 @@
+package com.sap.ai.sdk.foundationmodels.openai;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+import lombok.SneakyThrows;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.io.entity.InputStreamEntity;
+import org.junit.jupiter.api.Test;
+
+public class StreamConverterTest {
+  @SneakyThrows
+  @Test
+  void testStreamLines() {
+    final var TEMPLATE = "THIS\nIS\nA\nTEST\n";
+    final var input = TEMPLATE.repeat(StreamConverter.BUFFER_SIZE);
+    final var inputStream = spy(new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8)));
+    final var entity = new InputStreamEntity(inputStream, ContentType.TEXT_PLAIN);
+
+    final var sut = StreamConverter.streamLines(entity);
+
+    verify(inputStream, never()).read();
+    verify(inputStream, never()).read(any());
+    verify(inputStream, never()).read(any(), anyInt(), anyInt());
+
+    sut.forEach(
+        s -> assertThat(s).containsAnyOf("THIS", "IS", "A", "TEST").doesNotContainAnyWhitespaces());
+
+    verify(inputStream, times(TEMPLATE.length() + 1))
+        .read(any(), anyInt(), eq(StreamConverter.BUFFER_SIZE));
+  }
+}
