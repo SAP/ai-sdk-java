d83ef81c2d53df9c941e5d87798f297807c45a3c
I538344 <charles.dubois@sap.com>
2024-10-01 14:57:55 +0200

    Refactor test

--- sample-code/spring-app/src/test/java/com/sap/ai/sdk/app/controllers/ScenarioTest.java
+++ sample-code/spring-app/src/test/java/com/sap/ai/sdk/app/controllers/ScenarioTest.java
@@ -3,13 +3,11 @@ package com.sap.ai.sdk.app.controllers;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.sap.ai.sdk.core.client.model.AiModelBaseData;
-import com.sap.ai.sdk.core.client.model.AiModelList;
 import com.sap.ai.sdk.foundationmodels.openai.OpenAiModel;
-import io.vavr.control.Try;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
-
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,36 +15,30 @@ public class ScenarioTest {
 
   @Test
   @DisplayName("Declared OpenAI models must match AI Core's available OpenAI models")
+  @SneakyThrows
   public void openAiModelAvailability() {
 
-    // Gather the expected list of OpenAI models
-    final AiModelList aiModelListResponse = new ScenarioController().getModels();
-    assertThat(aiModelListResponse).isNotNull();
+    // Gather AI Core's list of available OpenAI models
+    final var aiModelList = new ScenarioController().getModels().getResources();
 
-    List<AiModelBaseData> aiModelList = aiModelListResponse.getResources();
-    assertThat(aiModelList).isNotNull().isNotEmpty();
+    final var availableOpenAiModels =
+        aiModelList.stream()
+            .filter(model -> model.getExecutableId().equals("azure-openai"))
+            .map(AiModelBaseData::getModel)
+            .toList();
 
-    List<String> expectedOpenAiModelList = new ArrayList<>();
-    for (AiModelBaseData model : aiModelList) {
-      if (model.getExecutableId().equals("azure-openai")) {
-        expectedOpenAiModelList.add(model.getModel());
-      }
-    }
+    // Gather our declared OpenAI models
+    Field[] declaredFields = OpenAiModel.class.getFields();
 
-    // Gather the declared OpenAI models
-    Field[] declaredFields = OpenAiModel.class.getDeclaredFields();
-
+    // get the models from the OpenAiModel class
     List<String> declaredOpenAiModelList = new ArrayList<>();
     for (Field field : declaredFields) {
       if (field.getType().equals(OpenAiModel.class)) {
-        OpenAiModel openAiModel = Try.of(() -> (OpenAiModel) field.get(null)).getOrNull();
-        if (openAiModel != null) {
-          declaredOpenAiModelList.add(openAiModel.model());
-        }
+        declaredOpenAiModelList.add(((OpenAiModel) field.get(null)).model());
       }
     }
 
     // Assert that the declared OpenAI models match the expected list
-    assertThat(declaredOpenAiModelList).hasSameElementsAs(expectedOpenAiModelList);
+    assertThat(declaredOpenAiModelList).hasSameElementsAs(availableOpenAiModels);
   }
 }
