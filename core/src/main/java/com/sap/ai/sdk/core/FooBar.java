762570502b18a8f7d90626ed4389cbb3fdea92c1
Alexander Dümont <alexander_duemont@web.de>
2024-10-24 15:40:28 +0200

    fix code

--- core/src/main/java/com/sap/ai/sdk/core/FooBar.java
+++ core/src/main/java/com/sap/ai/sdk/core/FooBar.java
@@ -1,13 +1,25 @@
 package com.sap.ai.sdk.core;
 
+import javax.annotation.Nonnull;
+
+/** A simple class to demonstrate the use of the SAP AI SDK. */
 public class FooBar {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
-    public static void foo() {
-        System.out.println("Foo");
-    }
-    public static void bar() {
-        System.out.println("Bar");
-    }
+  /**
+   * The main method.
+   *
+   * @param args The command-line arguments.
+   */
+  public static void main(final @Nonnull String[] args) {
+    System.out.println("Hello, world!");
+  }
+
+  /** A simple method to print "Foo". */
+  public void foo() {
+    System.out.println("Foo");
+  }
+
+  /** A simple method to print "Bar". */
+  protected static void bar() {
+    System.out.println("Bar");
+  }
 }
