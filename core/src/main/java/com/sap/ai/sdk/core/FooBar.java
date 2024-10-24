fbffc79ddaa74b8ad90f9a567fb7b90e90584bb5
Alexander Dümont <alexander_duemont@web.de>
2024-10-24 15:02:20 +0200

    Initial test

---
+++ core/src/main/java/com/sap/ai/sdk/core/FooBar.java
@@ -0,0 +1,13 @@
+package com.sap.ai.sdk.core;
+
+public class FooBar {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+    public static void foo() {
+        System.out.println("Foo");
+    }
+    public static void bar() {
+        System.out.println("Bar");
+    }
+}
