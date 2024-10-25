ba1a5758644c5c5981e1889728397295cda8ee4f
Matthias Kuhr <matthias.kuhr@sap.com>
2024-10-25 16:00:27 +0200

    Simplify Exception

--- orchestration/src/main/java/com/sap/ai/sdk/orchestration/OrchestrationClientException.java
+++ orchestration/src/main/java/com/sap/ai/sdk/orchestration/OrchestrationClientException.java
@@ -1,26 +1,7 @@
 package com.sap.ai.sdk.orchestration;
 
-import javax.annotation.Nonnull;
+import lombok.experimental.StandardException;
 
 /** Exception thrown by the {@link OrchestrationClient} in case of an error. */
-public class OrchestrationClientException extends RuntimeException {
-
-  /**
-   * Constructor.
-   *
-   * @param msg the error message
-   */
-  public OrchestrationClientException(@Nonnull final String msg) {
-    super(msg);
-  }
-
-  /**
-   * Constructor.
-   *
-   * @param msg the error message
-   * @param cause the cause of the error
-   */
-  public OrchestrationClientException(@Nonnull final String msg, @Nonnull final Throwable cause) {
-    super(msg, cause);
-  }
-}
+@StandardException
+public class OrchestrationClientException extends RuntimeException {}
