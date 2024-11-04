0d80f6dd682477284f311142c4757e04e94142c6
Matthias Kuhr <matthias.kuhr@sap.com>
2024-11-04 18:31:36 +0100

    Add Javadoc

--- orchestration/src/main/java/com/sap/ai/sdk/orchestration/OrchestrationConfig.java
+++ orchestration/src/main/java/com/sap/ai/sdk/orchestration/OrchestrationConfig.java
@@ -9,18 +9,49 @@ import lombok.Value;
 import lombok.With;
 
 /**
- * Default implementation of the {@link
- * com.sap.ai.sdk.orchestration.client.model.OrchestrationConfig} interface. This class supports
- * delegation from other classes by accepting a wrapper instance.
+ * Represents the configuration for the orchestration service. Allows for configuring the different
+ * modules of the orchestration service via a fluent API.
+ *
+ * <p>The orchestration pipeline combines different modules into a single execution flow where the
+ * output of one module serves as input for the next. The pipeline consists of the following
+ * modules:
+ *
+ * <ul>
+ *   <li>LLM Config (Mandatory)
+ *   <li>Templating (Optional)
+ *   <li>Data Masking (Optional)
+ *   <li>Input Content Filtering (Optional)
+ *   <li>Output Content Filtering (Optional)
+ * </ul>
  */
 @Value
 @With
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(force = true)
 public class OrchestrationConfig {
+  /**
+   * The configured language model settings. This configuration is required when executing requests.
+   */
   @Nullable AiModel llmConfig;
+
+  /**
+   * A template to be populated with input parameters. Upon request execution, this template will be
+   * enhanced with any messages and parameter values from {@link OrchestrationPrompt}.
+   */
   @Nullable TemplateConfig template;
+
+  /**
+   * A masking configuration to pseudonymous or anonymize sensitive data in the input.
+   */
   @Nullable MaskingConfig maskingConfig;
+
+  /**
+   * A content filter to filter the prompt.
+   */
   @Nullable ContentFilter inputContentFilter;
+
+  /**
+   * A content filter to filter the language model response.
+   */
   @Nullable ContentFilter outputContentFilter;
 }
