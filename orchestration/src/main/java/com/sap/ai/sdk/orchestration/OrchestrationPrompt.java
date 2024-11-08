0a98b1bbff1ddc92cedebba0f2407037a788d5b4
Matthias Kuhr <matthias.kuhr@sap.com>
2024-11-08 10:47:31 +0100

    fix PMD

--- orchestration/src/main/java/com/sap/ai/sdk/orchestration/OrchestrationPrompt.java
+++ orchestration/src/main/java/com/sap/ai/sdk/orchestration/OrchestrationPrompt.java
@@ -63,6 +63,7 @@ public class OrchestrationPrompt {
    *
    * @param messagesHistory The chat history to add.
    */
+  @Nonnull
   public OrchestrationPrompt messageHistory(@Nonnull final List<ChatMessage> messagesHistory) {
     this.messagesHistory.clear();
     this.messagesHistory.addAll(messagesHistory);
