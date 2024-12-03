5ecec8a4319b34d3bc6579f827bdbe79b4069487
Alexander Dümont <alexander_duemont@web.de>
2024-12-03 11:30:08 +0100

    Fix accessor

--- foundation-models/openai/src/main/java/com/sap/ai/sdk/foundationmodels/openai/model/OpenAiChatCompletionLogProbability.java
+++ foundation-models/openai/src/main/java/com/sap/ai/sdk/foundationmodels/openai/model/OpenAiChatCompletionLogProbability.java
@@ -3,8 +3,11 @@ package com.sap.ai.sdk.foundationmodels.openai.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import lombok.EqualsAndHashCode;
+import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.Accessors;
+
+import javax.annotation.Nullable;
 
 /** Log probability information for the choice. */
 @Accessors(chain = true)
@@ -13,5 +16,6 @@ import lombok.experimental.Accessors;
 public class OpenAiChatCompletionLogProbability {
   /** A list of message content tokens with log probability information. */
   @JsonProperty("content")
+  @Setter(onParam_ = @Nullable)
   private List<OpenAiChatCompletionLogProbabilityTop> content;
 }
