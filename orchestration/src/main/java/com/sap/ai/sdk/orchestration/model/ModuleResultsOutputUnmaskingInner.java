af0dd021de2253642e2a8fd6b804276b511c4353
Matthias Kuhr <52661546+MatKuhr@users.noreply.github.com>
2024-12-10 08:14:50 +0100

    Update with new generator version (#235)

--- orchestration/src/main/java/com/sap/ai/sdk/orchestration/model/ModuleResultsOutputUnmaskingInner.java
+++ orchestration/src/main/java/com/sap/ai/sdk/orchestration/model/ModuleResultsOutputUnmaskingInner.java
@@ -12,12 +12,13 @@
 
 package com.sap.ai.sdk.orchestration.model;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 /** ModuleResultsOutputUnmaskingInner */
-@JsonTypeInfo(
-    use = JsonTypeInfo.Id.NAME,
-    include = JsonTypeInfo.As.PROPERTY,
-    property = "",
-    visible = true)
+@JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION)
+@JsonSubTypes({
+  @JsonSubTypes.Type(value = LLMChoice.class),
+  @JsonSubTypes.Type(value = LLMChoiceStreaming.class),
+})
 public interface ModuleResultsOutputUnmaskingInner {}
