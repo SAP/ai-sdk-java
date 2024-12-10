8c3d1d1d6e88f051cf6f3fccd7b4da917ec02c5c
Jonas Israel <jonas.israel@sap.com>
2024-12-10 17:58:27 +0100

    Fourth option.

--- orchestration/src/main/java/com/sap/ai/sdk/orchestration/SystemMessage.java
+++ orchestration/src/main/java/com/sap/ai/sdk/orchestration/SystemMessage.java
@@ -1,12 +1,16 @@
 package com.sap.ai.sdk.orchestration;
 
 import javax.annotation.Nonnull;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
 import lombok.Value;
 import lombok.experimental.Accessors;
 
 /** Represents a chat message as 'system' to the orchestration service. */
 @Value
 @Accessors(fluent = true)
+@Getter(onMethod = @__(@JsonProperty))
 public class SystemMessage implements Message {
 
   /** The role of the assistant. */
--- orchestration/src/main/java/com/sap/ai/sdk/orchestration/UserMessage.java
+++ orchestration/src/main/java/com/sap/ai/sdk/orchestration/UserMessage.java
@@ -1,12 +1,16 @@
 package com.sap.ai.sdk.orchestration;
 
 import javax.annotation.Nonnull;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
 import lombok.Value;
 import lombok.experimental.Accessors;
 
 /** Represents a chat message as 'user' to the orchestration service. */
 @Value
 @Accessors(fluent = true)
+@Getter(onMethod = @__(@JsonProperty))
 public class UserMessage implements Message {
 
   /** The role of the assistant. */
