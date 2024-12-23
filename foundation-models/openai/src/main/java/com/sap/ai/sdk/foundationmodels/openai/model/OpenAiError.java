e173db5c63c6a7226cfc9d9b1cf87f796f0fde6b
Charles Dubois <103174266+CharlesDuboisSAP@users.noreply.github.com>
2024-12-23 16:14:22 +0100

    Reduce duplication of ResponseHandlers (#244)
    * Orchestration streaming first version
    
    * Added unit tests
    
    * Formatting
    
    * Added documentation
    
    * Added tests
    
    * Release notes
    
    * Applied Alex's review comments
    
    * Reduce duplication of ResponseHandlers
    
    * OpenAiStreamingHandler
    
    * fix spotbugs
    
    * Apply suggestions from code review
    
    * Formatting
    
    * Add Beta annotation to new public API
    
    * Minor code reduction
    
    * Formatting
    
    * Minor change
    
    ---------
    
    Co-authored-by: SAP Cloud SDK Bot <cloudsdk@sap.com>
    Co-authored-by: Alexander Dümont <22489773+newtork@users.noreply.github.com>
    Co-authored-by: Alexander Dümont <alexander_duemont@web.de>

--- foundation-models/openai/src/main/java/com/sap/ai/sdk/foundationmodels/openai/model/OpenAiError.java
+++ foundation-models/openai/src/main/java/com/sap/ai/sdk/foundationmodels/openai/model/OpenAiError.java
@@ -2,20 +2,23 @@ package com.sap.ai.sdk.foundationmodels.openai.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.annotations.Beta;
+import com.sap.ai.sdk.core.commons.ClientError;
 import javax.annotation.Nullable;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 import lombok.experimental.Accessors;
+import lombok.experimental.Delegate;
 
 /** OpenAI error. */
 @Accessors(chain = true)
 @EqualsAndHashCode
 @ToString
 @Beta
-public class OpenAiError {
+public class OpenAiError implements ClientError {
   /** The error object. */
   @JsonProperty("error")
   @Getter(onMethod_ = @Nullable)
+  @Delegate(types = {ClientError.class})
   private OpenAiErrorBase error;
 }
