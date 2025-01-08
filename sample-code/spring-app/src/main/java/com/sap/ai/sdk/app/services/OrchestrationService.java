931bb2ec14a593be608c7b2324ad5b6c89f7add1
Jonas Israel <jonas.israel@sap.com>
2025-01-08 14:43:50 +0100

    Clean up OrchestrationService for demo

--- sample-code/spring-app/src/main/java/com/sap/ai/sdk/app/services/OrchestrationService.java
+++ sample-code/spring-app/src/main/java/com/sap/ai/sdk/app/services/OrchestrationService.java
@@ -2,6 +2,7 @@ package com.sap.ai.sdk.app.services;
 
 import static com.sap.ai.sdk.orchestration.OrchestrationAiModel.GEMINI_1_5_FLASH;
 import static com.sap.ai.sdk.orchestration.OrchestrationAiModel.GPT_35_TURBO;
+import static com.sap.ai.sdk.orchestration.OrchestrationAiModel.Parameter.TEMPERATURE;
 
 import com.sap.ai.sdk.orchestration.DpiMasking;
 import com.sap.ai.sdk.orchestration.Message;
@@ -20,10 +21,71 @@ import javax.annotation.Nonnull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-/** Service class for the Orchestration service */
+/** Service class for the Demo app */
 @Service
 @Slf4j
 public class OrchestrationService {
+
+
+
+
+  @Nonnull
+  public String processInput(@Nonnull final String userInput) {
+    var client = new OrchestrationClient();
+    var config = new OrchestrationModuleConfig().withLlmConfig(GPT_35_TURBO.withParam(TEMPERATURE, 0));
+    var prompt = new OrchestrationPrompt(userInput);
+    var maskingConfig = DpiMasking.anonymization().withEntities(DPIEntities.LOCATION);
+    var response = client.chatCompletion(prompt, config.withMaskingConfig(maskingConfig));
+    return response.getContent();
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   /**
    * For Demo app.
    * Uses grounding to be able to answer question about AI SDK features.
@@ -53,58 +115,5 @@ public class OrchestrationService {
     final var configWithGrounding = config.withGroundingConfig(groundingConfig);
 
     return client.chatCompletion(prompt, configWithGrounding).getContent();
-  }
-
-  /**
-   * For Demo app.
-   * Starting point for demo app.
-   *
-   * @return the assistant response
-   */
-  @Nonnull
-  public String processInput00(@Nonnull final String userInput) {
-    return userInput;
-  }
-
-  /**
-   * For Demo app.
-   * First step for demo app, using the Orchestration service without features.
-   *
-   * @return the assistant response
-   */
-  @Nonnull
-  public String processInput01(@Nonnull final String userInput) {
-    final var config = new OrchestrationModuleConfig().withLlmConfig(GEMINI_1_5_FLASH);
-    final var client = new OrchestrationClient();
-    final var prompt = new OrchestrationPrompt(userInput);
-    return client.chatCompletion(prompt, config).getContent();
-  }
-
-  /**
-   * For Demo app.
-   * Second step for demo app, using the Orchestration service with masking.
-   *
-   * @return the assistant response
-   */
-  @Nonnull
-  public String processInput02(@Nonnull final String userInput) {
-    final var config = new OrchestrationModuleConfig().withLlmConfig(GEMINI_1_5_FLASH);
-    final var client = new OrchestrationClient();
-    final var prompt = new OrchestrationPrompt(userInput);
-    final var maskingConfig = DpiMasking.anonymization().withEntities(DPIEntities.LOCATION);
-    return client.chatCompletion(prompt, config.withMaskingConfig(maskingConfig)).getContent();
-  }
-
-  @Nonnull
-  public String processInput(@Nonnull final String userInput) {
-//    temp auf 0
-//    zoom ausprobieren und auf Bildschrimgröße anpassen
-//    Schrift art ändern und vergrößern in App
-    var client = new OrchestrationClient();
-    var config = new OrchestrationModuleConfig().withLlmConfig(GPT_35_TURBO);
-    var prompt = new OrchestrationPrompt(userInput);
-    var maskingConfig = DpiMasking.anonymization().withEntities(DPIEntities.LOCATION);
-    var response = client.chatCompletion(prompt, config.withMaskingConfig(maskingConfig));
-    return response.getContent();
   }
 }
