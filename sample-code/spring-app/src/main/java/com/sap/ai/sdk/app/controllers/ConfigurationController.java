689fd646b34d5d0eb7a46d1fe6032198a0d477d8
Jonas Israel <jonas.israel@sap.com>
2025-01-09 14:45:59 +0100

    WiP (fixed Scenario and Config endpoints)

--- sample-code/spring-app/src/main/java/com/sap/ai/sdk/app/controllers/ConfigurationController.java
+++ sample-code/spring-app/src/main/java/com/sap/ai/sdk/app/controllers/ConfigurationController.java
@@ -1,8 +1,17 @@
 package com.sap.ai.sdk.app.controllers;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+// import com.fasterxml.jackson.datatype.*;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.sap.ai.sdk.core.client.ConfigurationApi;
 import com.sap.ai.sdk.core.model.AiConfigurationList;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 /** Endpoint for Configuration operations */
@@ -11,6 +20,10 @@ import org.springframework.web.bind.annotation.RestController;
 class ConfigurationController {
 
   private static final ConfigurationApi CLIENT = new ConfigurationApi();
+  private final ObjectMapper mapper =
+      new ObjectMapper()
+          .setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
+          .registerModule(new JavaTimeModule());
 
   /**
    * Get the list of configurations.
@@ -18,7 +31,24 @@ class ConfigurationController {
    * @return the list of configurations
    */
   @GetMapping("/configurations")
-  AiConfigurationList getConfigurations() {
-    return CLIENT.query("default");
+  ResponseEntity<String> getConfigurations(
+      @RequestHeader(value = "accept", required = false) final String accept)
+      throws JsonProcessingException {
+    var configList = CLIENT.query("default");
+    if ("application/json".equals(accept)) {
+      return ResponseEntity.ok()
+          .contentType(MediaType.APPLICATION_JSON)
+          .body(mapper.writeValueAsString(configList));
+    }
+    return ResponseEntity.ok(buildMessage(configList));
+  }
+
+  String buildMessage(AiConfigurationList configList) {
+    var message = new StringBuilder("The following configurations are available: ");
+    for (var resource : configList.getResources()) {
+      message.append(resource.getName()).append(", ");
+    }
+    message.setCharAt(message.length() - 2, '.');
+    return message.toString();
   }
 }
--- sample-code/spring-app/src/main/java/com/sap/ai/sdk/app/controllers/ScenarioController.java
+++ sample-code/spring-app/src/main/java/com/sap/ai/sdk/app/controllers/ScenarioController.java
@@ -1,10 +1,19 @@
 package com.sap.ai.sdk.app.controllers;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.sap.ai.sdk.core.client.ScenarioApi;
 import com.sap.ai.sdk.core.model.AiModelList;
 import com.sap.ai.sdk.core.model.AiScenarioList;
 import javax.annotation.Nonnull;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 /** Endpoint for Scenario operations */
@@ -12,7 +21,13 @@ import org.springframework.web.bind.annotation.RestController;
 @SuppressWarnings("unused") // debug method that doesn't need to be tested
 class ScenarioController {
 
+//  TODO: Fix javadocs
+
   private static final ScenarioApi CLIENT = new ScenarioApi();
+  private final ObjectMapper mapper =
+      new ObjectMapper()
+          .setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
+          .registerModule(new JavaTimeModule());
 
   /**
    * Get the list of available scenarios
@@ -21,8 +36,21 @@ class ScenarioController {
    */
   @GetMapping("/scenarios")
   @Nonnull
-  AiScenarioList getScenarios() {
-    return CLIENT.query("default");
+  ResponseEntity<String> getScenarios(
+      @RequestHeader(value = "accept", required = false) final String accept)
+      throws JsonProcessingException {
+    var scenarioList = CLIENT.query("default");
+    if ("application/json".equals(accept)) {
+      return ResponseEntity.ok()
+          .contentType(MediaType.APPLICATION_JSON)
+          .body(mapper.writeValueAsString(scenarioList));
+    }
+    return ResponseEntity.ok(buildScenarioMessage(scenarioList));
+  }
+
+  @Nonnull
+  AiModelList getModels() {
+    return CLIENT.queryModels("foundation-models", "default");
   }
 
   /**
@@ -32,7 +60,33 @@ class ScenarioController {
    */
   @GetMapping("/models")
   @Nonnull
-  AiModelList getModels() {
-    return CLIENT.queryModels("foundation-models", "default");
+  ResponseEntity<String> getModels(
+      @RequestHeader(value = "accept", required = false) final String accept)
+      throws JsonProcessingException {
+    var modelList = CLIENT.queryModels("foundation-models", "default");
+    if ("application/json".equals(accept)) {
+      return ResponseEntity.ok()
+          .contentType(MediaType.APPLICATION_JSON)
+          .body(mapper.writeValueAsString(modelList));
+    }
+    return ResponseEntity.ok(buildModelMessage(modelList));
+  }
+
+  String buildScenarioMessage(AiScenarioList scenarioList) {
+    var message = new StringBuilder("The following scenarios are available: ");
+    for (var resource : scenarioList.getResources()) {
+      message.append(resource.getName()).append(", ");
+    }
+    message.setCharAt(message.length() - 2, '.');
+    return message.toString();
+  }
+
+  String buildModelMessage(AiModelList modelList) {
+    var message = new StringBuilder("The following models are available: ");
+    for (var resource : modelList.getResources()) {
+      message.append(resource.getModel()).append(", ");
+    }
+    message.setCharAt(message.length() - 2, '.');
+    return message.toString();
   }
 }
