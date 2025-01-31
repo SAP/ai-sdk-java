35859c088fd9e555b4aa508c23ece0b7001fe890
Roshin Rajan Panackal <roshin.rajan.panackal@sap.com>
2025-01-31 15:18:45 +0100

    Include embedding convenience

--- foundation-models/openai/src/main/java/com/sap/ai/sdk/foundationmodels/openai/OpenAiEmbeddingsRequestFactory.java
+++ foundation-models/openai/src/main/java/com/sap/ai/sdk/foundationmodels/openai/OpenAiEmbeddingsRequestFactory.java
@@ -9,14 +9,28 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
+/** Factory class for creating OpenAI Embedding Requests. */
 @Accessors(fluent = true)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OpenAiEmbeddingsRequestFactory {
 
+  /**
+   * Creates an EmbeddingsCreateRequest from a list of input strings.
+   *
+   * @param input the list of input strings
+   * @return an EmbeddingsCreateRequest with the provided input
+   */
   public static EmbeddingsCreateRequest fromList(@Nonnull List<String> input) {
     return new EmbeddingsCreateRequest().input(EmbeddingsCreateRequestInput.create(input));
   }
 
+  /**
+   * Creates an EmbeddingsCreateRequest from a single input string and additional input strings.
+   *
+   * @param input the first input string
+   * @param inputs additional input strings
+   * @return an EmbeddingsCreateRequest with the combined input strings
+   */
   public static EmbeddingsCreateRequest fromStrings(
       @Nonnull String input, @Nonnull String... inputs) {
     var inputList = new ArrayList<String>();
