683cfeb16f5fe149a5542504a65b4785328cd174
I538344 <charles.dubois@sap.com>
2025-02-20 13:34:37 +0100

    Added sample code endpoint

---
+++ sample-code/spring-app/src/main/java/com/sap/ai/sdk/app/controllers/PromptRegistryController.java
@@ -0,0 +1,20 @@
+package com.sap.ai.sdk.app.controllers;
+
+import com.sap.ai.sdk.prompt.registry.client.DefaultApi;
+import com.sap.ai.sdk.prompt.registry.model.PromptTemplateListResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/** Endpoint for Prompt Registry operations */
+@SuppressWarnings("unused") // debug class that doesn't need to be tested
+@RestController
+@RequestMapping("/prompt-registry")
+class PromptRegistryController {
+  private static final DefaultApi client = new DefaultApi();
+
+  @GetMapping("/listTemplates")
+  public PromptTemplateListResponse listTemplates() {
+    return client.listPromptTemplates();
+  }
+}
