0d5406969684347f4963e324ff4f518d9e4c3b5f
Roshin Rajan Panackal <36329474+rpanackal@users.noreply.github.com>
2025-02-25 13:50:50 +0100

    feat: [OpenAI] Integrate Messaging Convenience (#326)
    * Full openapi generated code for openai module
    
    * Fix rearrangement of code
    
    * Convenience level separated out
    
    * Reflect deprecation of direct string input on client with unit tests
    
    * E2e for both new and old apis
    
    - controller linked to new openai service only
    
    * Increasing test coverage
    
    * Test coverage for tool
    
    * Fix type
    
    * PR changes
    
    - Move Jackson object initialization to field declaration
    - update `streamChatCompletionDeltas` in `NewOpenAiService` to use basic string message
    - `@deprecated` tag on deprecated `chatCompletion` doc
    - `@Deprecated` annotation on embedding api in client
    - `OpenAiController` streamChatCompletionDeltas emits usage
    
    * Remove docs on OpenAi controller
    
    * Remove docs on OpenAi controller
    
    * tag public api with `@since`
    
    - make jackson mixin package private
    
    * Clean up test code for better separation and reduce repetition
    
    * Adapt tool test for better usecase
    
    * Remove deprecation annotation and java doc tag
    
    * Beta annotation on all new classes and client methods returning/taking unstable classes
    
    * Fix changes for new generated model class location
    
    * Update @since because of latest release
    
    * Convert request and response classes to Value classes
    
    * Fix @With equality check for Boolean
    
    * Move NewOpenAiService to test
    
    - Use Old OpenAiService
    - Adapt tests
    
    * utility class introduced for mapping to low level request message
    
    * Remove embedding convenience from PR
    
    * Method renaming
    
    * Charles suggested changes
    - Enums over string values in test
    - missed getContent assertion
    - replicate history test in old api test
    - Remove `@Value` from OpenAiChatCompletionDelta
    
    * Method renaming and make OpenAiChatCompletionDelta constructor access package private
    
    * Method renaming, add utility method for jackson and change constructor visibility
    
    - follow createX naming format over toX.
    - move openai object mapper construction logic to utility method
    - make OpenAiChatCompletionDelta constructor access package private
    
    * Improve one plus list construction
    
    * Make new delta test more readable
    
    - remove docs for overriding methods
    
    * Improve java doc in client and request
    
    * Remove the redundant test for usage in low level
    
    * Minimum integration of messaging convenience
    
    - Add user, assistant and system conv on par with orchestration
    - Adapt sample app service class for image input (new api)
    - Adapt toDTO to account for OpenAI gen model architecture
    
    * Add tool message class
    
    * Clean up test code for better separation and reduce repetition
    
    * Improve messaging throw behaviour and test coverage
    
    - Throw on unsupported content item
    - improve code quality user message to DTO mapper
    - Add unit tests for messaging convenience api
    
    * PMD suggestions
    
    * Increase test coverage - tool message test
    
    * `@since` annotation
    
    * Rebase fix and make message class constructors package private
    
    * Fix access for createChatCompletionRequestMessage in tool message
    
    * Renaming new classes in sample code
    
    * PR review suggested changes
    - Message constructors are package private and chained
    - Remove unnecessary method level `@since` annotation
    
    * PR Suggestions
    - improve docs
    - fix toolmessage to include toolCallId
    - reduce toolmessage to single text
    - improve error message
    - include test for immutability in messages
    
    * improve javadocs
    
    ---------
    
    Co-authored-by: Roshin Rajan Panackal <roshin.rajan.panackal@sap.com>

---
+++ foundation-models/openai/src/main/java/com/sap/ai/sdk/foundationmodels/openai/OpenAiImageItem.java
@@ -0,0 +1,63 @@
+package com.sap.ai.sdk.foundationmodels.openai;
+
+import com.google.common.annotations.Beta;
+import java.util.Locale;
+import javax.annotation.Nonnull;
+
+/**
+ * Represents an image item in a {@link OpenAiMessageContent} object.
+ *
+ * @param imageUrl the URL of the image
+ * @param detailLevel the detail level of the image (optional)
+ * @since 1.4.0
+ */
+@Beta
+public record OpenAiImageItem(@Nonnull String imageUrl, @Nonnull DetailLevel detailLevel)
+    implements OpenAiContentItem {
+
+  /**
+   * Creates a new image item with the given image URL.
+   *
+   * @param imageUrl the URL of the image
+   */
+  public OpenAiImageItem(@Nonnull final String imageUrl) {
+    this(imageUrl, DetailLevel.AUTO);
+  }
+
+  /** The detail level of the image. */
+  public enum DetailLevel {
+    /** Low detail level. */
+    LOW("low"),
+    /** High detail level. */
+    HIGH("high"),
+    /** Automatic detail level. */
+    AUTO("auto");
+
+    private final String level;
+
+    /**
+     * Converts a string to a detail level.
+     *
+     * @param str the string to convert
+     * @return the detail level
+     */
+    @Nonnull
+    static DetailLevel fromString(@Nonnull final String str) {
+      return DetailLevel.valueOf(str.toUpperCase(Locale.ENGLISH));
+    }
+
+    /**
+     * Get the string representation of the DetailLevel
+     *
+     * @return the DetailLevel as string
+     */
+    @Nonnull
+    public String toString() {
+      return level;
+    }
+
+    DetailLevel(@Nonnull final String level) {
+      this.level = level;
+    }
+  }
+}
--- foundation-models/openai/src/main/java/com/sap/ai/sdk/foundationmodels/openai/OpenAiMessage.java
+++ foundation-models/openai/src/main/java/com/sap/ai/sdk/foundationmodels/openai/OpenAiMessage.java
@@ -1,6 +1,7 @@
 package com.sap.ai.sdk.foundationmodels.openai;
 
 import com.google.common.annotations.Beta;
+import java.util.List;
 import javax.annotation.Nonnull;
 
 /**
@@ -9,38 +10,79 @@ import javax.annotation.Nonnull;
  * @since 1.4.0
  */
 @Beta
-public interface OpenAiMessage {
+public sealed interface OpenAiMessage
+    permits OpenAiUserMessage, OpenAiAssistantMessage, OpenAiSystemMessage, OpenAiToolMessage {
 
   /**
    * A convenience method to create a user message.
    *
-   * @param msg the message content.
+   * @param message the message content.
    * @return the user message.
    */
   @Nonnull
-  static OpenAiMessage user(@Nonnull final String msg) {
-    return new OpenAiUserMessage(msg);
+  static OpenAiUserMessage user(@Nonnull final String message) {
+    return new OpenAiUserMessage(message);
+  }
+
+  /**
+   * A convenience method to create a user message containing only an image.
+   *
+   * @param openAiImageItem the message content.
+   * @return the user message.
+   */
+  @Nonnull
+  static OpenAiUserMessage user(@Nonnull final OpenAiImageItem openAiImageItem) {
+    return new OpenAiUserMessage(new OpenAiMessageContent(List.of(openAiImageItem)));
   }
 
   /**
    * A convenience method to create an assistant message.
    *
-   * @param msg the message content.
+   * @param message the message content.
    * @return the assistant message.
    */
   @Nonnull
-  static OpenAiMessage assistant(@Nonnull final String msg) {
-    return new OpenAiAssistantMessage(msg);
+  static OpenAiAssistantMessage assistant(@Nonnull final String message) {
+    return new OpenAiAssistantMessage(message);
   }
 
   /**
    * A convenience method to create a system message.
    *
-   * @param msg the message content.
+   * @param message the message content.
    * @return the system message.
    */
   @Nonnull
-  static OpenAiMessage system(@Nonnull final String msg) {
-    return new OpenAiSystemMessage(msg);
+  static OpenAiSystemMessage system(@Nonnull final String message) {
+    return new OpenAiSystemMessage(message);
   }
+
+  /**
+   * A convenience method to create a tool message.
+   *
+   * @param message the message content.
+   * @param toolCallId identifier of the tool call this message is responding to.
+   * @return the tool message.
+   */
+  @Nonnull
+  static OpenAiToolMessage tool(@Nonnull final String message, @Nonnull final String toolCallId) {
+    return new OpenAiToolMessage(message, toolCallId);
+  }
+
+  /**
+   * Returns the role associated with the message.
+   *
+   * @return the role.
+   */
+  @Nonnull
+  String role();
+
+  /**
+   * Returns the content of the message.
+   *
+   * @return the content.
+   */
+  @Beta
+  @Nonnull
+  OpenAiMessageContent content();
 }
