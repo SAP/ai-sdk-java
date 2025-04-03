544a0679028eea397feca3ee829106a595dd091c
Charles Dubois <103174266+CharlesDuboisSAP@users.noreply.github.com>
2025-04-03 09:27:24 +0200

    feat: [PromptRegistry] Release module (#400)
    * feat: [Prompt Registry] Release module
    
    * import template in export
    
    * createUpdateTemplate
    
    * description
    
    * spring approved ✅
    
    * remove export 🚫
    
    * history test
    
    * remove warning
    
    * PMD

--- sample-code/spring-app/src/main/java/com/sap/ai/sdk/app/controllers/PromptRegistryController.java
+++ sample-code/spring-app/src/main/java/com/sap/ai/sdk/app/controllers/PromptRegistryController.java
@@ -2,7 +2,20 @@ package com.sap.ai.sdk.app.controllers;
 
 import com.sap.ai.sdk.prompt.registry.PromptClient;
 import com.sap.ai.sdk.prompt.registry.client.DefaultApi;
+import com.sap.ai.sdk.prompt.registry.model.PromptTemplateDeleteResponse;
 import com.sap.ai.sdk.prompt.registry.model.PromptTemplateListResponse;
+import com.sap.ai.sdk.prompt.registry.model.PromptTemplatePostRequest;
+import com.sap.ai.sdk.prompt.registry.model.PromptTemplatePostResponse;
+import com.sap.ai.sdk.prompt.registry.model.PromptTemplateSpec;
+import com.sap.ai.sdk.prompt.registry.model.PromptTemplateSubstitutionRequest;
+import com.sap.ai.sdk.prompt.registry.model.PromptTemplateSubstitutionResponse;
+import com.sap.ai.sdk.prompt.registry.model.SingleChatTemplate;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +25,78 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/prompt-registry")
 class PromptRegistryController {
+  static final String NAME = "java-e2e-test";
   private static final DefaultApi client = new PromptClient();
 
   @GetMapping("/listTemplates")
   PromptTemplateListResponse listTemplates() {
     return client.listPromptTemplates();
+  }
+
+  @GetMapping("/createTemplate")
+  PromptTemplatePostResponse createTemplate() {
+    return client.createUpdatePromptTemplate(getTemplate("Finance, Tech, Sports"));
+  }
+
+  @GetMapping("/updateTemplate")
+  PromptTemplatePostResponse updateTemplate() {
+    // create template then update
+    client.createUpdatePromptTemplate(getTemplate("Finance, Tech, Sports"));
+    return client.createUpdatePromptTemplate(getTemplate("Finance, Tech, Sports, Politics"));
+  }
+
+  private PromptTemplatePostRequest getTemplate(final String categories) {
+    final var spec =
+        PromptTemplateSpec.create()
+            .template(
+                SingleChatTemplate.create()
+                    .role("system")
+                    .content(
+                        "You classify input text into the two following categories: {{?categories}}"),
+                SingleChatTemplate.create().role("user").content("{{?inputExample}}"))
+            .defaults(Map.of("categories", categories));
+
+    return PromptTemplatePostRequest.create()
+        .name(NAME)
+        .version("0.0.1")
+        .scenario("categorization")
+        .spec(spec);
+  }
+
+  @GetMapping("/history")
+  PromptTemplateListResponse history() {
+    return client.listPromptTemplateHistory("categorization", "0.0.1", NAME);
+  }
+
+  @GetMapping("/importTemplate")
+  PromptTemplatePostResponse importTemplate() throws IOException {
+    final Resource template = new ClassPathResource("prompt-template.yaml");
+    return client.importPromptTemplate(template.getFile());
+  }
+
+  @GetMapping("/exportTemplate")
+  File exportTemplate() throws IOException {
+    final var template = importTemplate();
+    return client.exportPromptTemplate(template.getId());
+  }
+
+  @GetMapping("/useTemplate")
+  PromptTemplateSubstitutionResponse useTemplate() {
+    final var template = createTemplate();
+    return client.parsePromptTemplateById(
+        template.getId(),
+        false,
+        PromptTemplateSubstitutionRequest.create()
+            .inputParams(Map.of("inputExample", "I love football")));
+  }
+
+  @GetMapping("/deleteTemplate")
+  List<PromptTemplateDeleteResponse> deleteTemplate() {
+    final PromptTemplateListResponse templates = client.listPromptTemplates();
+
+    return templates.getResources().stream()
+        .filter(template -> NAME.equals(template.getName()))
+        .map(template -> client.deletePromptTemplate(template.getId()))
+        .toList();
   }
 }
