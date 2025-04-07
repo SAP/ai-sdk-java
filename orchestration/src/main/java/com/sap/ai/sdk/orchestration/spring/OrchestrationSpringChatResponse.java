95a61f398ef962b2d7ab39f272cda24cfb600112
I750911 <till.kurek@sap.com>
2025-04-07 15:34:48 +0200

    Revert "detection of output filter as finish reason"
    This reverts commit 88f552a595162a5a544121236318c8fb697f17c3.

--- orchestration/src/main/java/com/sap/ai/sdk/orchestration/spring/OrchestrationSpringChatResponse.java
+++ orchestration/src/main/java/com/sap/ai/sdk/orchestration/spring/OrchestrationSpringChatResponse.java
@@ -2,7 +2,6 @@ package com.sap.ai.sdk.orchestration.spring;
 
 import com.google.common.annotations.Beta;
 import com.sap.ai.sdk.orchestration.OrchestrationChatResponse;
-import com.sap.ai.sdk.orchestration.OrchestrationClientException;
 import com.sap.ai.sdk.orchestration.model.LLMChoice;
 import com.sap.ai.sdk.orchestration.model.LLMModuleResultSynchronous;
 import com.sap.ai.sdk.orchestration.model.TokenUsage;
@@ -49,12 +48,7 @@ public class OrchestrationSpringChatResponse extends ChatResponse {
   }
 
   @Nonnull
-  static Generation toGeneration(@Nonnull final LLMChoice choice)
-      throws OrchestrationClientException {
-    if ("content_filter".equals(choice.getFinishReason())) {
-      throw new OrchestrationClientException("Content filter filtered the output.");
-    }
-
+  static Generation toGeneration(@Nonnull final LLMChoice choice) {
     val metadata = ChatGenerationMetadata.builder().finishReason(choice.getFinishReason());
     metadata.metadata("index", choice.getIndex());
     if (!choice.getLogprobs().isEmpty()) {
