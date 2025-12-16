7b2c2d5f9881938e2464936ac3506fce36bccf02
Jonas Israel <jonas.israel@sap.com>
2025-12-16 10:04:34 +0100

    exception type changed

--- sample-code/spring-app/src/test/java/com/sap/ai/sdk/app/controllers/OrchestrationTest.java
+++ sample-code/spring-app/src/test/java/com/sap/ai/sdk/app/controllers/OrchestrationTest.java
@@ -507,7 +507,7 @@ class OrchestrationTest {
             () ->
                 client.chatCompletion(
                     prompt, filterConfig.withLlmConfig(GPT_5.withName("wrong-model"))))
-        .isExactlyInstanceOf(OrchestrationClientException.class)
+        .isInstanceOf(OrchestrationClientException.class)
         .hasMessageContaining("400")
         .hasMessageContaining("Model name must be one of");
 
