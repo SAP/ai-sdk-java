83d9515b68dc7c782684163941f2427d4f7d7dc9
I538344 <charles.dubois@sap.com>
2026-04-20 09:49:33 +0200

    wip client

---
+++ core-services/batch/src/main/java/com/sap/ai/sdk/batch/generated/BatchClient.java
@@ -0,0 +1,3 @@
+package com.sap.ai.sdk.batch.generated;
+
+public class BatchClient {}
--- sample-code/spring-app/src/main/java/com/sap/ai/sdk/app/controllers/BatchController.java
+++ sample-code/spring-app/src/main/java/com/sap/ai/sdk/app/controllers/BatchController.java
@@ -1,8 +1,14 @@
 package com.sap.ai.sdk.app.controllers;
 
 import com.sap.ai.sdk.batch.generated.client.BatchesApi;
+import com.sap.ai.sdk.batch.generated.model.BatchCreateRequest;
+import com.sap.ai.sdk.batch.generated.model.BatchCreateRequest.TypeEnum;
+import com.sap.ai.sdk.batch.generated.model.BatchCreateRequestInput;
+import com.sap.ai.sdk.batch.generated.model.BatchCreateRequestOutput;
+import com.sap.ai.sdk.batch.generated.model.BatchCreateRequestSpec;
 import com.sap.ai.sdk.batch.generated.model.BatchListResponse;
 import com.sap.ai.sdk.core.AiCoreService;
+import lombok.val;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +22,13 @@ public class BatchController {
 
   @GetMapping("/test")
   public BatchListResponse test() {
+    val a =
+        CLIENT.createBatch(
+            BatchCreateRequest.create()
+                .type(TypeEnum.LLM_NATIVE)
+                .input(BatchCreateRequestInput.create().uri(""))
+                .output(BatchCreateRequestOutput.create().uri(""))
+                .spec(BatchCreateRequestSpec.create().provider("").model("")));
     return CLIENT.listBatches();
   }
 }
