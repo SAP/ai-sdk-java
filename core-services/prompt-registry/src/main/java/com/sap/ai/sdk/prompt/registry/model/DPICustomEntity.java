0584d6b2beba5a7f4d180ded72b91b3aa6280893
Roshin Rajan Panackal <roshin.rajan.panackal@sap.com>
2026-05-06 17:32:06 +0200

    Regenerate prompt registry module

--- core-services/prompt-registry/src/main/java/com/sap/ai/sdk/prompt/registry/model/DPICustomEntity.java
+++ core-services/prompt-registry/src/main/java/com/sap/ai/sdk/prompt/registry/model/DPICustomEntity.java
@@ -75,7 +75,7 @@ public class DPICustomEntity implements DPIEntityConfig
    * Set the replacementStrategy of this {@link DPICustomEntity} instance and return the same
    * instance.
    *
-   * @param replacementStrategy The replacementStrategy of this {@link DPICustomEntity}
+   * @param replacementStrategy Replacement strategy to be used for the entity
    * @return The same instance of this {@link DPICustomEntity} class
    */
   @Nonnull
@@ -85,7 +85,7 @@ public class DPICustomEntity implements DPIEntityConfig
   }
 
   /**
-   * Get replacementStrategy
+   * Replacement strategy to be used for the entity
    *
    * @return replacementStrategy The replacementStrategy of this {@link DPICustomEntity} instance.
    */
@@ -97,7 +97,7 @@ public class DPICustomEntity implements DPIEntityConfig
   /**
    * Set the replacementStrategy of this {@link DPICustomEntity} instance.
    *
-   * @param replacementStrategy The replacementStrategy of this {@link DPICustomEntity}
+   * @param replacementStrategy Replacement strategy to be used for the entity
    */
   public void setReplacementStrategy(@Nonnull final DPIMethodConstant replacementStrategy) {
     this.replacementStrategy = replacementStrategy;
@@ -229,7 +229,7 @@ public class DPICustomEntity implements DPIEntityConfig
     /**
      * Set the replacementStrategy of this {@link DPICustomEntity} instance.
      *
-     * @param replacementStrategy The replacementStrategy of this {@link DPICustomEntity}
+     * @param replacementStrategy Replacement strategy to be used for the entity
      * @return The DPICustomEntity instance.
      */
     DPICustomEntity replacementStrategy(@Nonnull final DPIMethodConstant replacementStrategy);
